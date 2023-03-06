33eb4aed9af828c54ab37bd59f417bc731f7ff68
Baidyanath Dutta <mr.smart001@gmail.com>
2023-03-06 22:45:27 +0530

    rest commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,23 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
+
 export class AppComponent implements OnInit{
 
   fields: any[] = [];
-  data: any[] = [];
+  respData: any[] = [];
+ 
+  //parameters: { RESOURCE_ID: number }[] = [];
+  parameterObject: IParameterObject = {
+    PARAMETERS: []
+  };
+
+  parameterObject2: IParameterObject2 = {
+    PARAMETERS: []
+  };
   constructor(private dataService: DataService, private http: HttpClient) { }
 
 
@@ -24,12 +37,68 @@ ngOnInit(): void {
     
 }
 
+generateJSON(input: any): string {
+  const myJSON = JSON.stringify(input);
+  console.log(myJSON); // JSON object as a string
+  return myJSON;
+}
 
 getData() {
-    this.dataService.getData();
-  }
-  setData() {
+    // this.fields.forEach( element => {   
+    //   this.parameters.push({ "RESOURCE_ID": element.RESOURCE_ID });      
+    // })
 
+    for (let i = 0; i < this.fields.length; i++) {
+      const param: IParameter = {
+        RESOURCE_ID: this.fields[i].RESOURCE_ID
+      };
+      this.parameterObject.PARAMETERS.push(param);
+    }
+
+    var req  = this.generateJSON(this.parameterObject);
+
+    var resp = this.dataService.getUpdatedData(req);
   }
+
+
+setData() {
+
+  for (let i = 0; i < this.fields.length; i++) {
+    const param2: IParameter2 = {
+      RESOURCE_ID: this.fields[i].RESOURCE_ID,
+      PARAM_VALUE: this.fields[i].DISP_DEFAULT_VALUE
+    };
+    this.parameterObject2.PARAMETERS.push(param2);
+  }
+
+  var req = this.generateJSON(this.parameterObject2);
+
+  var resp = this.dataService.updateDataOnServer(req);
+
+}
+
+
+
   title = 'Device Param App';
+}
+
+interface IDataField {
+  RESOURCE_ID: number;
+  DISP_NAME: string;
+  DISP_UNIT_STRING: string;
+  DISP_DEFAULT_VALUE: string;
+}
+
+interface IParameter {
+  RESOURCE_ID: number;
+}
+interface IParameter2 {
+  RESOURCE_ID: number;
+  PARAM_VALUE: number;
+}
+interface IParameterObject {
+  PARAMETERS: IParameter[];
+}
+interface IParameterObject2 {
+  PARAMETERS: IParameter2[];
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://localhost:8080/api/GET'; // Replace with your API endpoint URL
+  // private apiUrl = 'http://localhost:8080/api/GET'; // Replace with your API endpoint URL
+  private apiUrl = 'http://localhost:8080/api';
   private constHeader = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
 
   fields: any[] = [];
@@ -19,11 +20,19 @@ export class DataService {
     }); // Use any[] if the response is an array of objects, otherwise use object{}
   }
 
-  postData(data: any): Observable<any> {
+  getUpdatedData(data: any): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
-    const url = `${this.apiUrl}/resource`;
+    const url = `${this.apiUrl}/GET`;
+    return this.http.post(this.apiUrl, data, { headers });
+  }
+
+  updateDataOnServer(data: any) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    const url = `${this.apiUrl}/SET`;
     return this.http.post(this.apiUrl, data, { headers });
   }
 }
